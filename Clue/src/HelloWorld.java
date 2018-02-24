b264906608c9ce28c4131df4a686e2d240660fed
Josh Fry <joshfry575@email.arizona.edu>
2018-02-23 21:46:19 -0700

    Created a HelloWorld class. Phase 0, practice Commit and Push

---
+++ Clue/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author joshfry
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Print text to the console window
+		System.out.println("Hello Mystery Inc!");
+
+	}
+
+}
